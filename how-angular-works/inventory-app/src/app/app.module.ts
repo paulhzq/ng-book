c68c8a4f188472167d944e9ebb4f4c0c0bed0466
Paul Huang <paul_hzq@hotmail.com>
2017-12-17 00:32:45 -0500

    add ng-book example code

---
+++ how-angular-works/inventory-app/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductImageComponent } from './product-image/product-image.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { Product } from './product.model';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ProductRowComponent,
+    ProductImageComponent,
+    PriceDisplayComponent,
+    ProductDepartmentComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [ ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ how-angular-works/inventory-app/src/app/product.model.ts
@@ -0,0 +1,11 @@
+// provide a 'Product' object
+
+export class Product {
+  constructor(
+    public sku: string,
+    public name: string,
+    public imageUrl: string,
+    public department: string[],
+    public price: number
+  ) {}
+}
